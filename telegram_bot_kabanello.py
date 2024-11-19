f1c1519ab1da07d5862184fb6eeebeaacd9328c9
AndrePim <andreo.obolensky@gmail.com>
2024-11-19 22:32:26 +0300

    add delete previous menu

--- telegram_bot_kabanello.py
+++ telegram_bot_kabanello.py
@@ -1,5 +1,6 @@
-from telegram import Update, BotCommand
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaAnimation, InputMediaAudio, Update, BotCommand
+import telegram
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 from datetime import datetime, timedelta
 import os
 import sys
@@ -15,7 +16,6 @@ MSK_TZ = pytz.timezone("Europe/Moscow")
 
 # Токен вашего бота
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-#TOKEN = "7874734421:AAGeb_aIRqo_uoesUUJxjl0jrLZ9m3L6moc"
 
 # Ссылки на гифки
 GIF_ON = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZjl4OTZiaWd4YjNoMGRobGR1YXk3eTJxdmNvMTAxOXhtb3o3bDY1eSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/XE5z1TYI2w5RddJigU/giphy.gif"  # Первая гифка для команды /kaban_off
@@ -37,123 +37,254 @@ GOOD_SHIFT_START_DATE = datetime(2024, 11, 1)
 
 # Функция для отправки гифки при команде /kaban_off
 async def kaban_off(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_animation(animation=GIF_OFF)
-
+    if update.callback_query:
+        await update.callback_query.edit_message_media(
+            media=InputMediaAnimation(GIF_OFF),
+            reply_markup=None
+        )
+    elif update.message:
+        await update.message.reply_animation(animation=GIF_OFF)
 
 # Функция для отправки гифки при команде /kaban_on
 async def kaban_on(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_animation(animation=GIF_ON)
+    if update.callback_query:
+        await update.callback_query.edit_message_media(
+            media=InputMediaAnimation(GIF_ON),
+            reply_markup=None
+        )
+    elif update.message:
+        await update.message.reply_animation(animation=GIF_ON)
 
+
+def get_day_word_form(days: int) -> str:
+    """Возвращает правильную форму слова 'день' в зависимости от числа."""
+    if 11 <= days % 100 <= 19:  # Исключение для 11-19
+        return "дней"
+    last_digit = days % 10
+    if last_digit == 1:
+        return "день"
+    elif 2 <= last_digit <= 4:
+        return "дня"
+    else:
+        return "дней"
 
 # Функция для команды /day_X, которая рассчитывает время до 15 ноября 2024 года
 async def day_x(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Текущее время
     now = datetime.now()
-    # Разница между целевой датой и текущим временем
     time_left = TARGET_DATE - now
-
-    # Проверяем, что дата не прошла
     if time_left.total_seconds() > 0:
-        # Извлекаем дни, часы, минуты и секунды
-        days = time_left.days
-        hours, remainder = divmod(time_left.seconds, 3600)
+        days, seconds = time_left.days, time_left.seconds
+        hours, remainder = divmod(seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
-
-        # Формируем сообщение
-        message = f"Времени до 15 ноября 2024 года:\n{days} дней, {hours} часов, {minutes} минут, {seconds} секунд"
+        day_word = get_day_word_form(days)
+        message = f"До 15 ноября 2024 года:\n{days} {day_word}, {hours} часов, {minutes} минут, {seconds} секунд"
     else:
-        message = "15 ноября 2024 года уже наступило!"
-
-    # Отправляем сообщение в чат
-    await update.message.reply_text(message)
-
+         # Расчет времени, прошедшего после 15 ноября 2024 года
+        days_passed = (now - TARGET_DATE).days
+        day_word = get_day_word_form(days_passed)
+        message = f"15 ноября 2024 года уже наступило!\nПрошло {days_passed} {day_word}, а ещё никто не уволился."
+    # Проверяем, откуда пришёл запрос, и отвечаем соответственно
+    if update.callback_query:
+        # Убираем кнопки и оставляем только текст
+        await update.callback_query.edit_message_text(text=message)
 
 # Команда /good_coffee
 async def good_coffee(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Текущее московское время
     now_msk = datetime.now(MSK_TZ)
-    today_msk = now_msk.date()
+    days_since_start = (now_msk.date() - GOOD_SHIFT_START_DATE.date()).days
+    animation = GIF_COFFEE_GOOD if days_since_start % 4 < 2 else GIF_COFFEE_BAD
 
-    # Рассчитываем количество дней с начала "хорошей" смены
-    days_since_good_shift_start = (today_msk - GOOD_SHIFT_START_DATE.date()).days
+    if update.callback_query:
+        await update.callback_query.edit_message_media(
+            media=InputMediaAnimation(animation),
+            reply_markup=None
+        )
+    elif update.message:
+        await update.message.reply_animation(animation=animation)
 
-    # Если количество дней с начала смены делится на 4 с остатком 0 или 1, смена "хорошая"
-    if days_since_good_shift_start % 4 < 2:
-        await update.message.reply_animation(animation=GIF_COFFEE_GOOD)
-    else:
-        await update.message.reply_animation(animation=GIF_COFFEE_BAD)
-
-
-async def restart(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Перезапуск бота...")
-    # restart_bot()
-
-# Функция для команды /rabota
+# 3. Аудио команды
 async def send_derevnja_durakov(
-    update: Update, context: ContextTypes.DEFAULT_TYPE
-) -> None:
-    # Выбоаудиофайла из папки (например, derevnja_durakov.mp3)
-    file_path = os.path.join(
-        SAMPLES_DIR, "derevnja_durakov.mp3"
-    )  # Замените "derevnja_durakov.mp3" на нужный файл
-    print(f"Путь к файлу: {file_path}")
-    print(f"Файл существует: {os.path.exists(file_path)}")
-
-    # Проверяем, существует ли файл
-    if os.path.exists(file_path):
-        # Отправляем аудиофайл
-        await update.message.reply_audio(audio=open(file_path, "rb"))
-    else:
-        # Если файл не найден, отправляем сообщение об ошибке
-        await update.message.reply_text("Аудиофайл не найден.")
+    update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await send_audio(update, "derevnja_durakov.ogg")
 
 
-# Функция для команды /z
 async def send_z(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Выбор аудиофайла из папки (например, goida.mp3)
-    file_path = os.path.join(
-        SAMPLES_DIR, "goida.mp3"
-    )  # Замените "goida.mp3" на нужный файл
-
-    # Проверка, существует ли файл
-    if os.path.exists(file_path):
-        # Отправка аудиофайл
-        await update.message.reply_audio(audio=open(file_path, "rb"))
-    else:
-        # Если файл не найден, отправляем сообщение об ошибке
-        await update.message.reply_text("Аудиофайл не найден.")
+    await send_audio(update, "goida.ogg")
 
 
-# Функция для команды /casino
 async def send_casino(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Выбираем аудиофайл из папки (например, casino.mp3)
-    file_path = os.path.join(
-        SAMPLES_DIR, "casino.mp3"
-    )  # Замените "casino.mp3" на нужный файл
+    await send_audio(update, "casino.ogg")
 
-    # Проверяем, существует ли файл
+
+async def send_dimon(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await send_audio(update, "dimon.ogg")
+
+
+async def send_daladna(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await send_audio(update, "daladna.ogg")
+
+async def send_audio(update: Update, file_name: str) -> None:
+    file_path = os.path.join(SAMPLES_DIR, file_name)
+
+    # Проверка существования файла
     if os.path.exists(file_path):
-        # Отправляем аудиофайл
-        await update.message.reply_audio(audio=open(file_path, "rb"))
+        # Убираем расширение .ogg из имени файла для заголовка
+        file_name_without_extension = os.path.splitext(file_name)[0]
+        caption = f"{file_name_without_extension}"
+
+        # Попытка удаления предыдущего сообщения
+        try:
+            if update.message:
+                await update.message.delete()
+            elif update.callback_query:
+                await update.callback_query.message.delete()
+        except Exception as e:
+            print(f"Не удалось удалить сообщение: {e}")
+
+        # Отправка нового сообщения с аудио
+        try:
+            if update.message:
+                await update.message.reply_audio(audio=open(file_path, "rb"), caption=caption)
+            elif update.callback_query:
+                await update.callback_query.answer()  # Убираем "часики" в кнопке
+                await update.callback_query.message.chat.send_audio(audio=open(file_path, "rb"), caption=caption)
+        except Exception as e:
+            print(f"Ошибка при отправке аудио: {e}")
     else:
-        # Если файл не найден, отправляем сообщение об ошибке
-        await update.message.reply_text("Аудиофайл не найден.")
+        error_text = "Аудиофайл не найден."
 
+        # Удаление предыдущего сообщения и отправка сообщения с ошибкой
+        try:
+            if update.message:
+                await update.message.delete()
+                await update.message.reply_text(error_text)
+            elif update.callback_query:
+                await update.callback_query.answer()  # Убираем "часики" в кнопке
+                await update.callback_query.message.chat.send_message(error_text)
+        except Exception as e:
+            print(f"Ошибка при отправке сообщения с ошибкой: {e}")
 
-async def set_bot_commands(application: Application):
-    await application.bot.set_my_commands(
+# Меню категорий команд
+async def menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    reply_markup = InlineKeyboardMarkup(
         [
-            BotCommand("kaban_off", "Природа очистилась"),
-            BotCommand("kaban_on", "Природа загрязнилась"),
-            BotCommand("day_x", "Отчёт времени до 15 ноября 2024 года"),
-            BotCommand("good_coffee", "Проверить, хорошая ли смена готовит кофе"),
-            BotCommand("vecherok", "Вечерок посидеть и готово"),
-            BotCommand("rabota", "Чёрт-ти что"),
-            BotCommand("z", "ГОЙДА"),
-            BotCommand("casino", "Этого казино"),
+            [InlineKeyboardButton("Время и смена", callback_data="time_menu")],
+            [InlineKeyboardButton("Природа", callback_data="nature_menu")],
+            [InlineKeyboardButton("Аудио команды", callback_data="audio_menu")],
         ]
     )
 
+    if update.callback_query:
+        await update.callback_query.edit_message_text(
+            text="Выберите категорию:", reply_markup=reply_markup
+        )
+    elif update.message:
+        await update.message.reply_text(
+            text="Выберите категорию:", reply_markup=reply_markup
+        )
+
+
+async def time_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    new_reply_markup = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("Время до 15 ноября", callback_data="day_x")],
+            [
+                InlineKeyboardButton(
+                    "Проверить смену для кофе", callback_data="good_coffee"
+                )
+            ],
+            [InlineKeyboardButton("Назад", callback_data="main_menu")],
+        ]
+    )
+
+    # Обновляем текст сообщения и кнопки, если оно отличается
+    try:
+        await query.edit_message_text(
+            text="Выберите категорию времени:", reply_markup=new_reply_markup
+        )
+    except telegram.error.BadRequest as e:
+        if "Message is not modified" not in str(e):
+            raise
+
+
+async def nature_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("Природа загрязнилась", callback_data="kaban_on")],
+            [InlineKeyboardButton("Природа очистилась", callback_data="kaban_off")],
+            [InlineKeyboardButton("Назад", callback_data="main_menu")],
+        ]
+    )
+
+    if update.callback_query:
+        await update.callback_query.edit_message_text(
+            text="Команды природы:", reply_markup=reply_markup
+        )
+    elif update.message:
+        await update.message.reply_text(
+            text="Команды природы:", reply_markup=reply_markup
+        )
+
+
+async def audio_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("Деревня дураков", callback_data="rabota")],
+            [InlineKeyboardButton("ГОЙДА", callback_data="z")],
+            [InlineKeyboardButton("Казино", callback_data="casino")],
+            [InlineKeyboardButton("Димон", callback_data="dimon")],
+            [InlineKeyboardButton("Назад", callback_data="main_menu")],
+        ]
+    )
+
+    if update.callback_query:
+        await update.callback_query.edit_message_text(
+            text="Аудио команды:", reply_markup=reply_markup
+        )
+    elif update.message:
+        await update.message.reply_text(
+            text="Аудио команды:", reply_markup=reply_markup
+        )
+
+# Обработчик нажатий кнопок
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+
+    # Удаление старых кнопок или сообщения
+    try:
+        await query.edit_message_reply_markup(reply_markup=None)
+    except telegram.error.BadRequest as e:
+        if "Message is not modified" not in str(e):
+            raise
+
+    if query.data == "main_menu":
+        await menu(update, context)
+    elif query.data == "time_menu":
+        await time_menu(update, context)
+    elif query.data == "nature_menu":
+        await nature_menu(update, context)
+    elif query.data == "audio_menu":
+        await audio_menu(update, context)
+    elif query.data == "day_x":
+        await day_x(update, context)
+    elif query.data == "good_coffee":
+        await good_coffee(update, context)
+    elif query.data == "kaban_on":
+        await kaban_on(update, context)
+    elif query.data == "kaban_off":
+        await kaban_off(update, context)
+    elif query.data == "rabota":
+        await send_derevnja_durakov(update, context)
+    elif query.data == "z":
+        await send_z(update, context)
+    elif query.data == "casino":
+        await send_casino(update, context)
+    elif query.data == "dimon":
+        await send_dimon(update, context)
+    elif query.data == "daladna":
+        await send_daladna(update, context)
 
 # Функция для команды /vecherok
 async def vecherok(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -164,19 +295,12 @@ def main():
     # Создания приложения для бота
     application = Application.builder().token(TOKEN).build()
 
-    # Установка команд бота в момент инициализации
-    application.post_init = set_bot_commands
+    # Основное меню
+    application.add_handler(CommandHandler("menu", menu))
 
-    # Добавление обработчики команд
-    application.add_handler(CommandHandler("kaban_off", kaban_off))
-    application.add_handler(CommandHandler("kaban_on", kaban_on))
-    application.add_handler(CommandHandler("day_x", day_x))
-    application.add_handler(CommandHandler("good_coffee", good_coffee))
-    application.add_handler(CommandHandler("restart", restart))
-    application.add_handler(CommandHandler("vecherok", vecherok))
-    application.add_handler(CommandHandler("rabota", send_derevnja_durakov))
-    application.add_handler(CommandHandler("z", send_z))
-    application.add_handler(CommandHandler("casino", send_casino))
+    # Обработчик нажатий кнопок
+    application.add_handler(CallbackQueryHandler(button_handler))
+
     # Запуск бота
     application.run_polling()
 
